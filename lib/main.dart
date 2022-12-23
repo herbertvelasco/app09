dd17a1456eee66b2dcff2ec254db9fdcbb7f96e5
HerbertVelasco <havm14@gmail.com>
2022-12-23 09:22:56 -0500

    Implementando home page

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Registro de Actividades',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Registro de Actividades',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> _journals = [];
+  bool _isLoading = true;
+  void _refreshJournals() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _journals = data;
+      _isLoading = false;
+    });
   }
 }
